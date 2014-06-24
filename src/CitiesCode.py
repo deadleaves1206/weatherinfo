8cbdd6006786c13f2b1a1a753d6884534571420f
ryli <deadleaves1206@gmail.com>
2014-06-24 20:28:12 +0800

    重写CitiesCode.p文件

--- src/CitiesCode.py
+++ src/CitiesCode.py
@@ -14,3 +14,19 @@
 #注：ElementTree对于恶意数据结构数据不能较好的处理
 
 #模块导入
+try:										#使用ET，对xml文件实现读写操作
+    import xml.etree.cElementTree as ET 	#使用C编译，更高效
+except ImportError:
+    import xml.etree.ElementTree as ET
+import getCitiesCode					#获取城市代码
+
+def readXML(in_path):
+	'读取xml文件'
+	tree = ET.parse(in_path)
+	return tree
+
+#测试代码，用于测试该模块所有功能是否正常
+if __name__ == '__main__':
+	import sys
+	type = sys.getfilesystemencoding()
+	print readXML.__doc__.decode('utf-8').encode(type)
